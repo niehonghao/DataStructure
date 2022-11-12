2319ae186d1def02a83b04f6cbdaede1c2988e0a
niehonghao <n1redh@163.com>
2022-11-12 18:10:34 +0800

    Recursion for Maze

---
+++ Recursion/src/Maze.java
@@ -0,0 +1,15 @@
+public class Maze {
+    public static void main(String[] args) {
+        //先创建一个二维数组，模拟迷宫
+
+        //地图
+        int[][] map = new int[8][7];
+        /**
+         *  使用1表示墙
+         *  上下全部置为1
+         */
+        for (int i = 0; i < 7; i++) {
+
+        }
+    }
+}
